cfb7f25c87892a66f05c6a56cd310ff7ef34c937
Anurodh Kanth <anurodhkanth976@gmail.com>
2026-03-23 17:17:11 +0545

    Add files via upload

---
+++ DSA/Stack_and_Queue/2.1.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+char Stack[20];
+int tos = -1;
+
+void push(char x){
+    Stack[++tos] = x;
+}
+
+char pop(){
+    if (tos == -1) {
+        return '\0';
+    } else {
+        return Stack[tos--];
+    }
+}
+
+int priority(char x)
+{
+    if (x == '(')
+    {
+        return 0;
+    }
+    if (x == '+' ||  x == '-'){
+        return 1;
+    }
+    if (x == '*' || x == '/')
+    {
+        return 2;
+    }
+}
+
+int main()
+{
+    char exp[20];
+    char *e, x;
+
+    printf("Enter the expression: \n");
+    scanf("%s", exp) ;
+    e = exp;
+
+    while (*e != '\0')               /* Denotes a null value */
+    {
+        if (isalnum(*e))
+        {
+            printf("%c",*e);
+        }
+        else if (*e == '(') {
+            push(*e);
+        }
+        else if (*e == ')')
+        {
+            while ((x = pop()) != '(')
+            {
+                printf("%c",x);
+            }
+        }
+        else
+        {
+            while (priority(Stack[tos]) >= priority(*e)) {
+                printf("%c",pop());
+            }
+            push(*e);
+        }
+        e++;
+    }
+
+    while (tos != -1)
+    {
+        putchar(pop());
+    }
+    
+    return 0;
+}
